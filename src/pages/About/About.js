f3101e5484f25fcdf1967e25fa1d2c98ae686bba
al-anas <alanas7171@gmail.com>
2022-10-19 16:48:42 +0600

    about component updated

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="container">
-      <h2 className="text-center">This is about page</h2>
+      <h2 className="text-center my-5">This is about page</h2>
     </div>
   );
 };
